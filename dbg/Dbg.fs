e24322d5064c777746b21d149b0954124662c69c
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-10-11 11:08:30 +0100

    dbg

---
+++ dbg/Dbg.fs
@@ -0,0 +1,20 @@
+﻿namespace global
+
+open System
+open System.Text
+
+[<AutoOpen>]
+module private Auto =
+    let inline sqr x = x * x
+
+module Dbg =
+    let Write (s:string) =
+        [|
+            "\u001b[32mDEBUG "
+            DateTime.Now.ToString("dd MMM HH:mm:ss.fffffff")
+            "> "
+            s
+            "\u001b[0m"
+        |]
+        |> String.Concat
+        |> stdout.WriteLine
